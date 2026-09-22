878c5a48af3dbe55ade0613815911d6bdca32dd5
Patrik Hallsjö <patrik.hallsjo@gmail.com>
2026-09-22 12:55:21 +0200

    Split accelerators into standalone modules

---
+++ accelerators/index.js
@@ -0,0 +1,4 @@
+import lakehouse from './aws-lakehouse.js';
+import fileChat from './file-grounded-chat.js';
+import reportFactory from './multi-agent-report-factory.js';
+export const ACCELERATORS=[lakehouse,fileChat,reportFactory];
